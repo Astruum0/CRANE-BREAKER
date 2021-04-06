650c2d752bec5c5492c9cec896ebf68cec2ed492
arthur.vella@ynov.com <60743111+Astruum0@users.noreply.github.com>
2021-04-06 10:38:11 +0200

    NPM init + First window

---
+++ index.js
@@ -0,0 +1,20 @@
+const electron = require("electron");
+const url = require("url");
+const path = require("path");
+
+const { app, BrowserWindow } = electron;
+
+let timerWindow;
+let settingsWindow;
+
+app.on("ready", () => {
+    timerWindow = new BrowserWindow({ titleBarStyle: "hidden" });
+    timerWindow.loadURL(
+        url.format({
+            pathname: path.join(__dirname, "views", "index.html"),
+            protocol: "file",
+            slashes: true,
+        })
+    );
+    timerWindow.maximize();
+});
