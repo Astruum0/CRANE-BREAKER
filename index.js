62d78137df2ce9ac0b3124c54e6ea00d41ba8a7c
Romain Ranaivoson <romain.ranaivoson@ynov.com>
2021-04-20 09:15:57 +0200

    added minwidth and maxwidth

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function redirection(link) {
 }
 
 app.on("ready", () => {
-    timerWindow = new BrowserWindow({ titleBarStyle: "hidden" });
+    timerWindow = new BrowserWindow({ titleBarStyle: "hidden", minWidth: 600, minHeight: 800 });
     timerWindow.loadURL(
         url.format({
             pathname: path.join(__dirname, "views", "index.html"),
@@ -20,6 +20,6 @@ app.on("ready", () => {
             slashes: true,
         })
     );
-    timerWindow.removeMenu()
+    timerWindow.removeMenu();
     timerWindow.maximize();
 });
